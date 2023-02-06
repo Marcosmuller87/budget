88ff6abf1cc7dadb5ec93daf85ae59ee824e6da5
Marcos Muller <marcos.muller@magmati.com.br>
2023-02-06 14:52:06 -0300

    Basic Structure finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExpensesComponent } from './components/expenses/expenses.component';
+import { EnterBudgetComponent } from './components/enter-budget/enter-budget.component';
+import { EnterExpensesComponent } from './components/expenses/enter-expenses/enter-expenses.component';
+import { ExpensesListComponent } from './components/expenses/expenses-list/expenses-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpensesComponent,
+    EnterBudgetComponent,
+    EnterExpensesComponent,
+    ExpensesListComponent
   ],
   imports: [
     BrowserModule,
